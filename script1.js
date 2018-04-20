d25ffb7deb08da1a6505d3615aa9f8f61550086e
gollomotors <rhoerlykgmail.com>
2018-04-20 15:19:40 +0200

    initial commit

---
+++ script1.js
@@ -0,0 +1,7 @@
+$('.cqc-footer-trigger-btn').click(function(){
+  $(this).toggleClass('is-open');
+});
+
+// $('.example-3 .btn').click(function(){
+//  $(this).parents('.layout').find('.alert').toggleClass('is-open');
+// });
